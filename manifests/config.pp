3230dcad5841af1976ff50ea6a1bac51fabc3d7a
Paul Edmon <pedmon@cfa.harvard.edu>
2022-11-01 15:40:07 -0400

    Trying this.

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@ class nvidia::config {
     enable  => true,
     ensure  => 'running',
     require => [
-      File['/usr/lib/systemd/system/nvidia-persistenced.service'],
+      Package['cuda-drivers'],
     ],
   }
 }
