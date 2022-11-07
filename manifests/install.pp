daf480ed39fbe464ef15765b21d2d1ed2a94b4ab
Paul Edmon <pedmon@cfa.harvard.edu>
2022-11-07 11:24:20 -0500

    Fixing this to a resource.

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,6 +4,11 @@ class nvidia::install (
   package { 'cuda-drivers':
     ensure  => $version,
     require => Yumrepo['cuda'],
-    notify  => Exec['/usr/sbin/dkms autoinstall -m nvidia'],
+    notify  => Exec['build-dkms-nvidia-module'],
+  }
+
+  exec { 'build-dkms-nvidia-module':
+    command => '/usr/sbin/dkms autoinstall -m nvidia',
+    refreshonly => true,
   }
 }
