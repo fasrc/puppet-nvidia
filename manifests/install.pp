a5e018f0ed3b0c43bdbd7ad1513da334ae3f9e98
Paul Edmon <pedmon@cfa.harvard.edu>
2024-09-05 11:19:08 -0400

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@ class nvidia::install (
   String $version         = 'installed',
   String $gdrcopy_version = 'installed',
   Array  $ucx_pkgs        = ['ucx-cuda','ucx-gdrcopy'],
-  Array  $nvidia_packages = ['nvidia-driver','nvidia-driver-cuda','nvidia-settings','nvidia-xconfig','nvidia-libXNVCtrl-devel','nvidia-persistenced','cuda-driver','nvidia-driver-NVML'],
+  Array  $nvidia_packages = ['nvidia-driver','nvidia-driver-cuda','nvidia-settings','nvidia-xconfig','nvidia-libXNVCtrl-devel','nvidia-persistenced','nvidia-driver-NVML'],
   String $ucx_version     = 'present',
 ){
   package { 'kmod-nvidia-latest-dkms':
