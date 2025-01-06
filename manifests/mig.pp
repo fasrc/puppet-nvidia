feb90360b9116e1ffe3ecff4fde0661a7d4b3b82
Paul Edmon <pedmon@DESKTOP-5GBIA4B>
2025-01-06 10:24:37 -0500

    Adding symlinks for MIG config.

--- manifests/mig.pp
+++ manifests/mig.pp
@@ -15,11 +15,25 @@ class nvidia::mig (
     notify  => Service['nvidia-mig-manager'],
   }
 
+  file { '/etc/nvidia-mig-manager/config.yaml' :
+    ensure  => link,
+    target  => '/etc/nvidia-mig-manager/config-default.yaml',
+    require => Package['nvidia-mig-manager'],
+  }
+
+  file { '/etc/nvidia-mig-manager/hooks.yaml' :
+    ensure  => link,
+    target  => '/etc/nvidia-mig-manager/hooks-default.yaml',
+    require => Package['nvidia-mig-manager'],
+  }
+
   service { 'nvidia-mig-manager':
     enable  => true,
     require => [
       Package['nvidia-mig-manager'],
       File['/etc/systemd/system/nvidia-mig-manager.service.d/override.conf'],
+      File['/etc/nvidia-mig-manager/config.yaml'],
+      File['/etc/nvidia-mig-manager/hooks.yaml'],
     ],
   }
 }
