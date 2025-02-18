dbcbb993798074f375f1b2e30719d14bd126d959
Paul Edmon <pedmon@cfa.harvard.edu>
2025-02-18 10:42:58 -0500

    Should be 25.1.0

--- manifests/nsight.pp
+++ manifests/nsight.pp
@@ -2,7 +2,7 @@
 class nvidia::nsight (
   String $version              = 'latest',
   String $systems_package_name = 'nsight-systems-2024.6.2',
-  String $compute_package_name = 'nsight-compute-2024.1.0',
+  String $compute_package_name = 'nsight-compute-2025.1.0',
 ){
   package { $systems_package_name:
     ensure => $version,
