2899ececd1ea9f05d7685542cf3ab4661c5a5c31
Paul Edmon <pedmon@cfa.harvard.edu>
2025-02-18 10:35:43 -0500

    Updating nsight packages.

--- manifests/nsight.pp
+++ manifests/nsight.pp
@@ -1,8 +1,13 @@
 # Installs nsight
 class nvidia::nsight (
-  $version = 'latest',
-  $package_name = 'nsight-systems-2022.4.2', ){
-  package { $package_name:
+  String $version              = 'latest',
+  String $systems_package_name = 'nsight-systems-2024.6.2',
+  String $compute_package_name = 'nsight-compute-2024.1.0',
+){
+  package { $systems_package_name:
+    ensure => $version,
+  }
+  package { $compute_package_name:
     ensure => $version,
   }
 }
